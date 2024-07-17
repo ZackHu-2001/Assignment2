0ec35899814e541354da6bb4713a29e44f577f99
zackhu's macbook air <zackhu2001@gmail.com>
2024-07-16 19:50:08 -0700

    update navigator

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StyleSheet, Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import ActivitiesScreen from './Screens/ActivitiesScreen';
-import DietScreen from './Screens/DietScreen';
-import SettingsScreen from './Screens/SettingsScreen';
+import ActivitiesScreen from './screens/ActivitiesScreen';
+import DietScreen from './screens/DietScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
+import AddActivitiesScreen from './screens/AddActivitiesScreen';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function TabNavigator() {
+  return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Activities') {
+              iconName = focused ? 'walk' : 'walk-outline';
+            } else if (route.name === 'Diet') {
+              iconName = focused ? 'nutrition' : 'nutrition-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Activities" component={ActivitiesScreen} />
+        <Tab.Screen name="Diet" component={DietScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-      <Tab.Screen name="Activities" component={ActivitiesScreen} />
-      <Tab.Screen name="Diet" component={DietScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="AddActivity" component={AddActivitiesScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
